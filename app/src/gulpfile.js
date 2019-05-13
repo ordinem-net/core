5a1605a3bababa9898f3f1d6a7917224fc2f8534
lazaryan <lazaryan99@gmail.com>
2019-05-14 01:03:47 +0300

    modify gulp for images

--- app/src/gulpfile.js
+++ app/src/gulpfile.js
@@ -15,6 +15,9 @@ const csso          = require('gulp-csso');
 const babel         = require('gulp-babel');
 const uglify        = require('gulp-uglify-es').default;
 
+/*images*/
+const imgmin        = require('gulp-imagemin');
+
 
 const IsDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV ==='development';
 
@@ -45,6 +48,14 @@ gulp.task('js', () => {
         .pipe(gulp.dest(`${DIST}/js`));
 });
 
+gulp.task('images', () => {
+    return gulp.src(`${SRC}/img/**/*.*`)
+        .pipe(gulpif(!IsDevelopment, imgmin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest(`${DIST}/img`));
+});
+
 gulp.task('clean', () => {
     return gulp.src([`${DIST}/js/**/*.js`, `${DIST}/css/**/*.css`], {read: false})
         .pipe(clean({force: true}));
@@ -53,7 +64,8 @@ gulp.task('clean', () => {
 gulp.task('watch', () => {
     watch(`${SRC}/styles/**/[^_]*.less`, gulp.series('styles'));
     watch(`${SRC}/js/**/*.js`, gulp.series('js'));
+    watch(`${SRC}/img/**/*.*`, gulp.series('images'));
 });
 
-gulp.task('default', gulp.series('clean', gulp.parallel('js', 'styles', 'watch')));
-gulp.task('prod', gulp.series('clean', gulp.parallel('js', 'styles')));
+gulp.task('default', gulp.series('clean', gulp.parallel('images', 'js', 'styles', 'watch')));
+gulp.task('prod', gulp.series('clean', gulp.parallel('images', 'js', 'styles')));
