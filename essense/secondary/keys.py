8a63f00a50ec6df7fff09f1323ab661caaf0cfa7
Dmitry Mingalev <d.mingalev99@gmail.com>
2019-05-15 11:34:11 +0300

    Update keys.py
    Add generate signature and verify public key.

--- essense/secondary/keys.py
+++ essense/secondary/keys.py
@@ -10,15 +10,15 @@ class Keys:
         Генерирует открытый и закрытый ключ и записывает их в файлы public_key.txt и private_key.txt
         """
         # Если будут ошибки в дальнейшем, то сменить на 512.
-        (__pubkey, __privkey) = rsa.newkeys(128)
+        (__pubkey, __privkey) = rsa.newkeys(512)
 
         __pubkey_file = open('public_key.txt', 'w')
         __privkey_file = open('private_key.txt', 'w')
 
-        __pubkey_file.write(str(__pubkey))
-        __privkey_file.write(str(__privkey))
+        __pubkey_file.write(__pubkey.save_pkcs1().decode('ascii'))
+        __privkey_file.write(__privkey.save_pkcs1().decode('ascii'))
 
-    def publickey_to_login(pubkey):
+    def pubkey_to_address(pubkey):
         """
         Метод преобразующий открытый ключ в адрес.
         В качестве параметра принимает открытый ключ и возвращает адрес в формате str.
@@ -28,8 +28,14 @@ class Keys:
 
         return address
 
-    def verify_key():
-        pass
+    def address_to_pubkey(address):
+        """
+        Метод преобразующий адрес в открытый ключ.
+        В качестве параметра принимает адрес и возвращает открытый ключ в формате str.
+        """
+        pubkey = base64.b64decode(address).decode('utf-8')
+
+        return pubkey
 
     def get_keys():
         """
@@ -39,6 +45,34 @@ class Keys:
         __pubkey_file = open('public_key.txt', 'r')
         __privkey_file = open('private_key.txt', 'r')
 
-        keys = [__pubkey_file.readline(), __privkey_file.readline()]
+        keys = [__pubkey_file.read(), __privkey_file.read()]
 
         return keys
+
+    def sign_test():
+        """
+        Метод подписывающий тестовое сообщение. Необходимо для дальнейшей проверки транзакции другими пользователями.
+        Возвращает подпись в формате str.
+        """
+        __to_privkey = Keys.get_keys()[1]
+        __privkey = rsa.PrivateKey.load_pkcs1(__to_privkey)
+        __message = '1970'
+
+        __to_signature = rsa.sign(
+            __message.encode('utf-8'), __privkey, 'SHA-1')
+        signature = base64.b64encode(__to_signature)
+        signature = signature.decode('utf-8')
+
+        return signature
+
+    def verify_pubkey(address, signature):
+        """
+        Метод подписывающий верифицирующий публичный ключ пользователя.
+        В случае успешной проверки, возвращает True.
+        """
+        __message = '1970'
+
+        pubkey = Keys.address_to_pubkey(address)
+
+        if ((rsa.verify(__message, signature, pubkey)) == 'SHA-1'):
+            return True
