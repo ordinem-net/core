87c2b4afd2fd7fc6f9bdff54d5f80014dfaf1c86
Dmitry Mingalev <d.mingalev99@gmail.com>
2019-05-23 22:19:49 +0300

    Add signature

--- essense/secondary/blockchain.py
+++ essense/secondary/blockchain.py
@@ -1,6 +1,9 @@
 from essense.secondary.transactions import Transactions
+from essense.secondary.keys import Keys
 import time
 import hashlib
+import rsa
+import base64
 
 class Blockchain:
 
@@ -26,11 +29,19 @@ class Blockchain:
             nonce += 1
             hash_nonce = hashlib.md5((str(hash) + str(nonce)).encode('UTF-8')).hexdigest()
         
+        pubkey = Keys().get_keys()[0]
+        privkey = Keys().get_keys()[1]
+        address = Keys().pubkey_to_address(pubkey)
+        
+        signature = base64.b64encode(rsa.sign(hash.encode('utf-8'), rsa.PrivateKey.load_pkcs1(privkey), 'SHA-1')).decode('utf-8')
+        
         block = {
             'timestamp' : timestamp,
             'transactions' : transactions,
             'hash' : hash,
-            'nonce': nonce
+            'nonce': nonce,
+            'miner' : address,
+            'signature' : signature
         }
         
         return block
@@ -43,6 +54,9 @@ class Blockchain:
         """
 
         transactions = block.get('transactions')
+        signature = block.get('signature')
+        miner = block.get('miner')
+        _pubkey = Keys.address_to_pubkey(miner)
         block_flag = 0
         
         for i in range (len(transactions)):
@@ -66,6 +80,13 @@ class Blockchain:
         hash_nonce = hashlib.md5((hash + str(nonce)).encode('UTF-8')).hexdigest()
         
         if hash_nonce[0:len(condition)] != condition:
+            return False
+        
+        try:
+            pubkey = rsa.PublicKey.load_pkcs1(_pubkey)
+            if ((rsa.verify(hash.encode('UTF-8'), base64.b64decode(signature), pubkey)) == 'SHA-1'):
+                return True
+        except:
             return False
         
         return True
