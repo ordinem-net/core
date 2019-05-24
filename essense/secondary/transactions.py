510be7ccc3d95ab8c7d8d21110c3e9c0bba55485
Dmitry Mingalev <d.mingalev99@gmail.com>
2019-05-25 00:10:11 +0300

    Update transactions

--- essense/secondary/transactions.py
+++ essense/secondary/transactions.py
@@ -31,7 +31,7 @@ class Transactions:
             'signature' : signature,
             'data' : data
         }
-        message = Network().create_message(address,type,transaction)
+        message = Network().create_message(address,'transaction',transaction)
         Network().send('127.0.0.1',const.PORT_USER,message)
         return True
 
